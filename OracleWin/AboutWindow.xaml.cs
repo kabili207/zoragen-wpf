35b3b20771df900f7b6df1bda3bf746b35c1cce9
Andrew Nagle <kabili@zyrenth.com>
2013-10-21 16:39:26 -0400

    Added homepage link on About window. Tweaked README.

--- OracleWin/AboutWindow.xaml.cs
+++ OracleWin/AboutWindow.xaml.cs
@@ -48,5 +48,11 @@ namespace Zyrenth.OracleHack.Wpf
 			}
 
 		}
+
+		private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+		{
+			Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+			e.Handled = true;
+		}
 	}
 }
